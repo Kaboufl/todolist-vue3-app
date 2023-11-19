fb23fa3d475c843220a7f636e89e1abff9bcb93e
Romain Laurence <mw3pumat8@gmail.com>
2023-11-19 01:41:16 +0100

    ajout d'un bouton supprimer un todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -14,7 +14,11 @@ const props = defineProps<{ todo: Todo }>()
  * Cela permet de notifier le composant parent que quelque chose se passe
  * (et l'y faire réagir)
  */
-const emits = defineEmits(['update:todo', 'enter'])
+const emits = defineEmits([
+                            'update:todo', 
+                            'delete:todo',
+                            'enter', 
+                          ])
 
 /**
  * Fonction appelée lorsque le texte dans l'input change.
@@ -58,6 +62,18 @@ const handleCheckboxChange = ($event: Event) => {
   */
   emits('update:todo', { ...props.todo, done: newValue })
 }
+
+/**
+ * Fonction appelée lorsque l'on clique sur le bouton de suppression.
+ * On va utiliser cette fonction pour émettre un évènement
+ * et notifier que l'on veut supprimer le Todo
+ */
+const deleteTodo = () => {
+  /*  on émet un évenement `delete:todo` qui va pouvoir être capté par le composant parent
+      on donne en paramètre l'id du todo à supprimer
+  */
+  emits('delete:todo', props.todo.id)
+}
 </script>
 
 <template>
@@ -89,12 +105,18 @@ const handleCheckboxChange = ($event: Event) => {
             pour réagir au changement
     -->
     <input
-      v-if="todo.displayDone"
+      v-if="todo.interact"
       type="checkbox"
       :checked="todo.done"
       :title="`Click to set ${todo.done ? 'not done' : 'done'}`"
       @change="handleCheckboxChange"
     />
+    <button v-if="todo.interact" class="deleteBtn" @click="deleteTodo">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+      </svg>
+
+    </button>
   </div>
 </template>
 
@@ -120,7 +142,23 @@ input.text-box {
   padding: 0.5rem;
   background-color: #dba113;
   display: flex;
-  align-items: baseline;
+  flex-direction: row;
+  gap: 1.25rem;
+  align-items: center;
+}
+
+.deleteBtn {
+  background-color: transparent;
+  border: none;
+  color: red;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  width: 1.2rem;
+  height: 1.2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .done {
